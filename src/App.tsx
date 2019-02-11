ecf6195e6e926f0b4c90045854b26b6ff624bfa9
Lindsay Loughlin <lindsayloughlin@gmail.com>
2019-02-12 09:27:47 +1100

    include front end start

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import './App.css';
+
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">World clocks</h1>
+        </header>
+        <p className="App-intro">
+          Place your component here!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
